6a2b3d5d4514b24e604a81aa081491a039e3edb6
giorgossideris <56915448+giorgossideris@users.noreply.github.com>
2021-04-12 19:17:51 +0300

    Add main method to HistogramGenerator class

--- ase2021_practical_assignments/gradeshistogram/src/main/java/HistogramGenerator.java
+++ ase2021_practical_assignments/gradeshistogram/src/main/java/HistogramGenerator.java
@@ -13,12 +13,27 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 /**
  * HistogramGenerator class contains the methods that read a file with grades
- * and create a Histogram.
+ * and create a Histogram and the main method of the project.
  * 
  * @author Giorgos Sideris
  */
 
 public class HistogramGenerator {
+	
+	/**
+	 * The main method of the project.
+	 * 
+	 * @param args: args[0] should contain the name of the file
+	 */
+	public static void main(String[] args) {
+		int[] grades = null;
+		try {
+			grades = HistogramGenerator.readGradesFile(args[0]); // read the file with the grades
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		HistogramGenerator.generateHistogram(grades); // generate the histogram with the grades
+	}
 
 	/**
 	 * This method is used to read a file with grades.
