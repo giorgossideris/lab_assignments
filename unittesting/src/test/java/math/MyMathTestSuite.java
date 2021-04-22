9f19de55d6555695c1aeed7e7b9e40e2be9ff6aa
giorgossideris <56915448+giorgossideris@users.noreply.github.com>
2021-04-22 15:00:10 +0300

    Add a suite that groups the tests for factorial method

---
+++ unittesting/src/test/java/math/MyMathTestSuite.java
@@ -0,0 +1,10 @@
+package math;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MyMathFactorialParameterizedTest.class, MyMathTest.class})
+public class MyMathTestSuite {
+	// This class remains empty, it is used only as a holder for the above annotations		
+}
