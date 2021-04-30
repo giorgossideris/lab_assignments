510fb1d5809f592e4e64af7baad425e2f1e7c4c6
giorgossideris <56915448+giorgossideris@users.noreply.github.com>
2021-04-30 15:39:49 +0300

    Add test cases for thrown Exceptions

--- unittesting/src/test/java/math/ArrayOperationsTest.java
+++ unittesting/src/test/java/math/ArrayOperationsTest.java
@@ -2,16 +2,19 @@ package math;
 
 import io.FileIO;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.Assert;
+import org.junit.Rule;
 
 import static org.mockito.Mockito.*;
 
 public class ArrayOperationsTest {
 
 	ArrayOperations arrayOperations = new ArrayOperations();
+	FileIO fileIO = new FileIO();
 	
 	@Test
-	public void testFindPrimesInFile() {
+	public void testFindPrimesInFileValid() {
 		
 		FileIO fio = mock(FileIO.class);
 		when(fio.readFile("numbers.txt")).thenReturn(new int[] {3, 55, 101, 24});
@@ -26,7 +29,26 @@ public class ArrayOperationsTest {
 		
 		int[] expectedNumbers = new int[] {3, 101};
 		Assert.assertArrayEquals(expectedNumbers, arrayOperations.findPrimesInFile(fio, "numbers.txt", mm));
-	
 	}
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void testFindPrimesInFileNonExistentFile() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Input file does not exist");
+		fileIO.readFile("src/test/resources/non_existent.txt");
+	}
+	
+	@Test
+	public void testFindPrimesInFileEmptyFile() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Given file does not contain any valid values");
+		fileIO.readFile("src/test/resources/empty.txt");
+	}
+	
+	
+	
 	
 }
