8afa79980a0d0180de5c1718f344e2ae61785957
giorgossideris <56915448+giorgossideris@users.noreply.github.com>
2021-06-12 12:04:18 +0300

    Add new DemoClient

--- sourcecodeanalyzer/src/main/java/demo/DemoClient.java
+++ sourcecodeanalyzer/src/main/java/demo/DemoClient.java
@@ -1,43 +1,29 @@
 package demo;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-import codeanalyzer.*;
+import facades.AnalyzeManagement;
 
-public class DemoClient {
-
+public class DemoClient2 {
 	public static void main(String[] args) throws IOException {
 		String filepath = "src/main/resources/TestClass.java";
 		String sourceCodeAnalyzerType = "regex";
 		String sourceFileLocation = "local";
-		String outputFilePath = "output_metrics";
+		String outputFilePath = "output_metrics2";
 		String outputFileType = "csv";
-		
-		if(args.length == 5) {
-			filepath = args[0];
-			sourceCodeAnalyzerType = args[1];
-			sourceFileLocation = args[2];
-			outputFilePath = args[3];
-			outputFileType = args[4];
+
+		 if(args.length == 5) { 
+			 filepath = args[0]; 
+			 sourceCodeAnalyzerType = args[1];
+			 sourceFileLocation = args[2];
+			 outputFilePath = args[3]; 
+			 outputFileType = args[4]; 
 		} else if (args.length != 0) {
-			System.out.println("Incorrect number of arguments.");
-			System.exit(1);
-		}
+			System.out.println("Incorrect number of arguments."); 
+			System.exit(1); 
+		 }
 
-		SourceCodeAnalyzer analyzer = new SourceCodeAnalyzer(sourceFileLocation);
-		int loc = analyzer.calculateLOC(filepath, sourceCodeAnalyzerType);
-		int nom = analyzer.calculateNOM(filepath, sourceCodeAnalyzerType);
-		int noc = analyzer.calculateNOC(filepath, sourceCodeAnalyzerType);
-		
-		Map<String, Integer> metrics = new HashMap<>();
-		metrics.put("loc",loc);
-		metrics.put("nom",nom);
-		metrics.put("noc",noc);
-				
-		MetricsExporter exporter = new MetricsExporter();
-		exporter.writeFile(outputFileType, metrics, outputFilePath);
+		AnalyzeManagement analysis = new AnalyzeManagement();
+		analysis.executeAnalysis(filepath, sourceCodeAnalyzerType, sourceFileLocation, outputFilePath, outputFileType);
 	}
-
 }
